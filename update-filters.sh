952e358b43eec129ee4643971842305e37190542
FiorenMas <102145692+FiorenMas@users.noreply.github.com>
2022-12-30 23:06:49 +0700

    Add files via upload

---
+++ update-filters.sh
@@ -0,0 +1,27 @@
+readonly -A FILTERS=(
+    ['adguard-easy.txt']='https://filters.adtidy.org/android/filters/2_optimized.txt'
+    ['adguard-mobile-ads.txt']='https://filters.adtidy.org/android/filters/11_optimized.txt'
+    ['adguard-tracking-protection.txt']='https://filters.adtidy.org/android/filters/3_optimized.txt'
+    ['abpvn.txt']='https://raw.githubusercontent.com/abpvn/abpvn/master/filter/abpvn.txt'
+    ['adguard-url-tracking.txt']='https://filters.adtidy.org/extension/ublock/filters/17_optimized.txt'
+)
+
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+for filter in "${!FILTERS[@]}"; do
+    url="${FILTERS[$filter]}"
+    echo "Downloading $filter from $url"
+    if ! curl -o "$filter" "$url"; then
+        echo "Failed to download $url"
+        exit 1
+    fi
+    echo
+done
+
+#join_by , "${!FILTERS[@]}"
+
+echo "Converting filters"
+./ruleset_converter --input_format=filter-list \
+                    --output_format=unindexed-ruleset \
+                    --input_files="$(join_by , "${!FILTERS[@]}")" \
+                    --output_file=filters.dat
